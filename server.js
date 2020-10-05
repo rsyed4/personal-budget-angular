85c97ac0e84140e53b2d71bcd389cf06f4baf828
Rashid Syed <rashidhsyed@gmail.com>
2020-10-04 20:18:37 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,23 +4,7 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 30
-        },
-        {
-            title: 'Rent',
-            budget: 350
-        },
-        {
-            title: 'Groceries',
-            budget: 90
-        }
-    ]
-};
-
+const budget = require('./myBudget.json');
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
